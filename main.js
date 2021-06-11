c8bfbfd86df774cdd547f3fe24da04c0b6066b5c
HungnguyenVN <hungnguyen.cnn@gmail.com>
2021-06-11 17:00:23 +0700

    BT

---
+++ main.js
@@ -0,0 +1,13 @@
+// let value = null;
+// do {
+//     value = +prompt("Nhập 1 số vào đây")
+// } while (value < 1 || value > 10);
+// alert(value);
+
+
+let value = null;
+while (value < 1 || value > 10)
+{
+    value = +prompt("Nhập 1 số vào đây")
+} 
+alert(value);
